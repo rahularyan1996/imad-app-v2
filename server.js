7dd953f93fc6aa65e43b6ff7cb2c9db221ce6cbe
rahularyan1996 <rahularyan1996@gmail.com>
2017-03-25 14:51:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ app.get('/', function (req, res) {
 app.get('/article-one', function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/article-two', function(req,res){
-res.send('Article two is here access.');
-});
-app.get('/article-three', function(req,res){
-res.send('Article three is here access.');
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
