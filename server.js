1d847c43267350026bd121276157f2caf4199801
rahularyan1996 <rahularyan1996@gmail.com>
2017-03-25 16:17:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
+var articles ={
+articleOne : {
     title:'Article One',
     heading:'My first Blog Article.',
     date: '25-03-2017',
@@ -21,6 +22,36 @@ var articleOne = {
                 </p>`
     
     
+},
+articleTwo :{
+    title:'Article Two',
+    heading:'My Second Blog Article.',
+    date: '26-03-2017',
+    content:` 
+                <p>We all have come across one person who has this amazing ability to know and understand you well. These people leave you with a lot of warmth and every interaction with them makes you feel more comfortable. They are positive and since they themselves are sensitive they communicate with care and are able to consider others feelings – they are what we call highly sensitive people.
+            </p>
+            <p>They are able to manage conflicts between people effectively. Why? Because they have a keen eye for observation and details, and their involved behavior helps them to diffuse the tensions quickly.
+            </p>
+            <p>Dhoni started making rounds for his cool attitude – something we all most aspire to emulate. We would find him with a smile even in the toughest of games. This calm attitude of his landed him as the Captain of the team. He had not only made his dream of playing alongside his idol Sachin a reality but had become his captain.
+                
+            </p>`
+    
+    
+},
+articleThree:{
+     title:'Article Three',
+    heading:'My Third Blog Article.',
+    date: '27-03-2017',
+    content:` 
+                <p>A lot of philanthropy and humanitarian work is a result of efforts made by highly sensitive individuals.These people have a sense of purpose and want to make a difference in others lives and are able to give up their comforts for the achieving the same.        
+            </p>
+            <p>They are intense people – they feel deeply for things and they do things with involvement and are passionate about what they want to do. They are perceptive and active and want to find ways to deal with the challenges.   
+            </p>
+           <p>They like to involve others and help them develop and grow in work. If you have made a mistake, instead of flaring up at you, they will actually try to help you understand your mistakes and instill confidence back in you.   
+            </p>`
+    
+    
+}
 };
 function createTemplate (data){
     var title=data.title;
